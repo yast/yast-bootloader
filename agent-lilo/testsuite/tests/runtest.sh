349df5afbe0697bcb38002a5ac5be502d18621af
Jiří Šrain <jsrain@suse.cz>
2002-08-28 12:05:51 +0000

    fixed building on SPARC removed unneeded files from GRUB installation fixed boot floppies location fixed logging
    svn path=/trunk/bootloader/; revision=5484

--- agent-lilo/testsuite/tests/runtest.sh
+++ agent-lilo/testsuite/tests/runtest.sh
@@ -3,7 +3,12 @@
 unset Y2DEBUG
 unset Y2DEBUGGER
 
+grep sparc site.exp >/dev/null 2>&1
+SPARC=$?
+
 IN_FILE=${1%.*}".in"
+test $SPARC = 0 && test -f ${1%.*}".sparc.in" && cp ${1%.*}".sparc.in" ${1%.*}".in"
+test $SPARC = 0 && test -f ${1%.*}".sparc.out" && cp ${1%.*}".sparc.out" ${1%.*}".out"
 rm -f "$IN_FILE.test" 2> /dev/null
 cp $IN_FILE "$IN_FILE.test" 2> /dev/null
 
