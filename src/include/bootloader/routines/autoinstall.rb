6a12df4665e72cce5071620e7a2b1e0ba1fbce13
Josef Reidinger <jreidinger@suse.cz>
2014-06-26 12:48:16 +0200

    add missing textdomain

--- src/include/bootloader/routines/autoinstall.rb
+++ src/include/bootloader/routines/autoinstall.rb
@@ -102,6 +102,7 @@ module Yast
       #   </sections>
       # </bootloader>
       # -------------------------------------------------
+      textdomain "bootloader"
 
       Yast.import "Bootloader"
       Yast.import "BootStorage"
