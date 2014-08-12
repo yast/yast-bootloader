1ec69d6820f72962ef01c3b475109ae2a9691435
Martin Vidner <mvidner@suse.cz>
2014-08-12 17:02:59 +0200

    tests rewritten for even more readability

--- test/boot_common_test.rb
+++ test/boot_common_test.rb
@@ -4,67 +4,72 @@ Yast.import "BootCommon"
 
 describe Yast::BootCommon do
   describe ".setKernelParamToLine" do
+    def expect_set(key: nil, val: nil, old: nil, new: nil)
+      expect(Yast::BootCommon.setKernelParamToLine(old, key, val)).
+        to eq new
+    end
+
     it "return line with added key=value if there is not yet such key on line" do
-      old_line = "quit silent=1"
-      new_line = "quit silent=1 vga=800"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "vga", "800")).
-        to eq new_line
+      expect_set(key: "vga",
+                 val: "800",
+                 old: "quit silent=1",
+                 new: "quit silent=1 vga=800")
     end
 
     it "return line with modified kernel parameter to given value if line contain key" do
-      old_line = "quit silent=1 vga=753"
-      new_line = "quit silent=1 vga=800"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "vga", "800")).
-        to eq new_line
+      expect_set(key: "vga",
+                 val: "800",
+                 old: "quit silent=1 vga=753",
+                 new: "quit silent=1 vga=800")
     end
 
     it "return line with added parameter to kernel parameter line if value is \"true\"" do
-      old_line = "quit silent=1 vga=753"
-      new_line = "quit silent=1 vga=753 verbose"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "true")).
-        to eq new_line
+      expect_set(key: "verbose",
+                 val: "true",
+                 old: "quit silent=1 vga=753",
+                 new: "quit silent=1 vga=753 verbose")
     end
 
     it "return same line if parameter is already on parameter line when value is \"true\"" do
-      old_line = "quit silent=1 vga=753"
-      new_line = "quit silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "quit", "true")).
-        to eq new_line
+      expect_set(key: "quit",
+                 val: "true",
+                 old: "quit silent=1 vga=753",
+                 new: "quit silent=1 vga=753")
     end
 
     it "return line with removed parameter from line if value is \"false\"" do
-      old_line = "quit silent=1 vga=753"
-      new_line = "silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "quit", "false")).
-        to eq new_line
+      expect_set(key: "quit",
+                 val: "false",
+                 old: "quit silent=1 vga=753",
+                 new: "silent=1 vga=753")
     end
 
     it "return same line if parameter is already missing on line when value is \"false\"" do
-      old_line = "quit silent=1 vga=753"
-      new_line = "quit silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "false")).
-        to eq new_line
+      expect_set(key: "verbose",
+                 val: "false",
+                 old: "quit silent=1 vga=753",
+                 new: "quit silent=1 vga=753")
     end
 
     it "return line with key=value if line is nil" do
-      old_line = nil
-      new_line = "silent=1"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "silent", "1")).
-        to eq new_line
+      expect_set(key: "silent",
+                 val: "1",
+                 old: nil,
+                 new: "silent=1")
     end
 
     it "return line with key when value is \"true\" and line is nil" do
-      old_line = nil
-      new_line = "verbose"
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "true")).
-        to eq new_line
+      expect_set(key: "verbose",
+                 val: "true",
+                 old: nil,
+                 new: "verbose")
     end
 
     it "return empty string when value is \"false\" and line is nil" do
-      old_line = nil
-      new_line = ""
-      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "false")).
-        to eq new_line
+      expect_set(key: "verbose",
+                 val: "false",
+                 old: nil,
+                 new: "")
     end
   end
 end
