a4cc760e96b46ab5ba7086357135df6b60b49371
Martin Vidner <mvidner@suse.cz>
2014-08-12 16:37:16 +0200

    tests reformatted to be slightly more readable

--- test/boot_common_test.rb
+++ test/boot_common_test.rb
@@ -5,66 +5,66 @@ Yast.import "BootCommon"
 describe Yast::BootCommon do
   describe ".setKernelParamToLine" do
     it "return line with added key=value if there is not yet such key on line" do
-      line = "quit silent=1"
-      result_line = "quit silent=1 vga=800"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "vga", "800")).to eq result_line
+      old_line = "quit silent=1"
+      new_line = "quit silent=1 vga=800"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "vga", "800")).
+        to eq new_line
     end
 
     it "return line with modified kernel parameter to given value if line contain key" do
-      line = "quit silent=1 vga=753"
-      result_line = "quit silent=1 vga=800"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "vga", "800")).to eq result_line
+      old_line = "quit silent=1 vga=753"
+      new_line = "quit silent=1 vga=800"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "vga", "800")).
+        to eq new_line
     end
 
     it "return line with added parameter to kernel parameter line if value is \"true\"" do
-      line = "quit silent=1 vga=753"
-      result_line = "quit silent=1 vga=753 verbose"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "verbose", "true")).to eq result_line
+      old_line = "quit silent=1 vga=753"
+      new_line = "quit silent=1 vga=753 verbose"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "true")).
+        to eq new_line
     end
 
     it "return same line if parameter is already on parameter line when value is \"true\"" do
-      line = "quit silent=1 vga=753"
-      result_line = "quit silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "quit", "true")).to eq result_line
+      old_line = "quit silent=1 vga=753"
+      new_line = "quit silent=1 vga=753"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "quit", "true")).
+        to eq new_line
     end
 
     it "return line with removed parameter from line if value is \"false\"" do
-      line = "quit silent=1 vga=753"
-      result_line = "silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "quit", "false")).to eq result_line
+      old_line = "quit silent=1 vga=753"
+      new_line = "silent=1 vga=753"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "quit", "false")).
+        to eq new_line
     end
 
     it "return same line if parameter is already missing on line when value is \"false\"" do
-      line = "quit silent=1 vga=753"
-      result_line = "quit silent=1 vga=753"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "verbose", "false")).to eq result_line
+      old_line = "quit silent=1 vga=753"
+      new_line = "quit silent=1 vga=753"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "false")).
+        to eq new_line
     end
 
     it "return line with key=value if line is nil" do
-      line = nil
-      result_line = "silent=1"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "silent", "1")).to eq result_line
+      old_line = nil
+      new_line = "silent=1"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "silent", "1")).
+        to eq new_line
     end
 
     it "return line with key when value is \"true\" and line is nil" do
-      line = nil
-      result_line = "verbose"
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "verbose", "true")).to eq result_line
+      old_line = nil
+      new_line = "verbose"
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "true")).
+        to eq new_line
     end
 
     it "return empty string when value is \"false\" and line is nil" do
-      line = nil
-      result_line = ""
-      expect(Yast::BootCommon.setKernelParamToLine(
-        line, "verbose", "false")).to eq result_line
+      old_line = nil
+      new_line = ""
+      expect(Yast::BootCommon.setKernelParamToLine(old_line, "verbose", "false")).
+        to eq new_line
     end
   end
 end
