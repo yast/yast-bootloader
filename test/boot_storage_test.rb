57921ee358ddf1fc7d6716477c46ea41e59ac06f
Josef Reidinger <jreidinger@suse.cz>
2014-09-11 17:30:02 +0200

    add test for MapDevices method

--- test/boot_storage_test.rb
+++ test/boot_storage_test.rb
@@ -120,4 +120,22 @@ describe Yast::BootStorage do
       ).to eq(result)
     end
   end
+
+  describe ".MapDevices" do
+    after do
+      Yast::BootStorage.all_devices = {}
+    end
+
+    it "fills BootStorage.all_devices with mapping of udev devices to kernel devices" do
+      target_map_stub("storage_ppc.rb")
+
+      Yast::BootStorage.MapDevices()
+      all_devices = Yast::BootStorage.all_devices
+      expect(all_devices["/dev/disk/by-id/ata-HITACHI_HTS723232A7A364_E3834563C86LDM-part1"]).to eq "/dev/sda1"
+      # test below demonstrate bug in method, which use wrong target map key
+      # expect(all_devices["/dev/disk/by-path/pci-0000:00:1f.2-scsi-0:0:0:0-part2"]).to eq "/dev/sda2"
+      # test below demonstrate problem when only first device is added to map
+      #expect(all_devices["/dev/disk/by-id/wwn-0x5000cca6d4c3bbb8"]).to eq "/dev/sda"
+    end
+  end
 end
