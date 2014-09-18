cea20da21f6d6500f677191decc19b359f723b3a
Josef Reidinger <jreidinger@suse.cz>
2014-09-18 13:18:15 +0200

    use constant for runnign shell scripts

--- src/lib/bootloader/backup_mbr.rb
+++ src/lib/bootloader/backup_mbr.rb
@@ -6,6 +6,8 @@ module Bootloader
   class BackupMBR
     class << self
       include Yast::Logger
+      BASH_PATH = Yast::Path.new(".target.bash")
+      BASH_OUTPUT_PATH = Yast::Path.new(".target.bash_output")
       # Creates backup of MBR or PBR of given device.
       # Backup is stored in /var/lib/YaST2/backup_boot_sectors, in logs
       # directory and if it is MBR of primary disk, then also in /boot/backup_mbr
@@ -17,7 +19,7 @@ module Bootloader
         )
         device_file_path_to_logs = Yast::Ops.add("/var/log/YaST2/", device_file)
         Yast::SCR.Execute(
-          Yast::Path.new(".target.bash"),
+          BASH_PATH,
           "test -d /var/lib/YaST2/backup_boot_sectors || mkdir /var/lib/YaST2/backup_boot_sectors"
         )
         if Yast::Ops.greater_than(Yast::SCR.Read(Yast::Path.new(".target.size"), device_file_path), 0)
@@ -47,12 +49,12 @@ module Bootloader
           end
           change_date = grub_getFileChangeDate(device_file_path)
           Yast::SCR.Execute(
-            Yast::Path.new(".target.bash"),
+            BASH_PATH,
             Yast::Builtins.sformat("/bin/mv %1 %1-%2", device_file_path, change_date)
           )
         end
         Yast::SCR.Execute(
-          Yast::Path.new(".target.bash"),
+          BASH_PATH,
           Yast::Builtins.sformat(
             "/bin/dd if=%1 of=%2 bs=512 count=1 2>&1",
             device,
@@ -61,7 +63,7 @@ module Bootloader
         )
         # save MBR to yast2 log directory
         Yast::SCR.Execute(
-          Yast::Path.new(".target.bash"),
+          BASH_PATH,
           Yast::Builtins.sformat(
             "/bin/dd if=%1 of=%2 bs=512 count=1 2>&1",
             device,
@@ -70,7 +72,7 @@ module Bootloader
         )
         if device == Yast::BootCommon.mbrDisk
           Yast::SCR.Execute(
-            Yast::Path.new(".target.bash"),
+            BASH_PATH,
             Yast::Builtins.sformat(
               "/bin/dd if=%1 of=%2 bs=512 count=1 2>&1",
               device,
@@ -83,7 +85,7 @@ module Bootloader
             device_file_path_thinkpad = Yast::Ops.add(device_file_path, "thinkpadMBR")
             log.info("Backup thinkpad MBR")
             Yast::SCR.Execute(
-              Yast::Path.new(".target.bash"),
+              BASH_PATH,
               Yast::Builtins.sformat(
                 "cp %1 %2 2>&1",
                 device_file_path,
@@ -105,7 +107,7 @@ module Bootloader
           "date --date='1970-01-01 00:00:00 %1 seconds' +\"%%Y-%%m-%%d-%%H-%%M-%%S\"",
           ctime
         )
-        out = Yast::Convert.to_map(Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), command))
+        out = Yast::Convert.to_map(Yast::SCR.Execute(BASH_OUTPUT_PATH, command))
         c_time = Yast::Ops.get_string(out, "stdout", "")
         log.debug("File #{filename}: last change #{c_time}")
         c_time
