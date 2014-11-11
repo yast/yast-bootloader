588763b859f69d5406d33c4f26b7416eba4ca923
Josef Reidinger <jreidinger@suse.cz>
2014-11-11 15:34:21 +0100

    DRY switcher

--- src/include/bootloader/routines/switcher.rb
+++ src/include/bootloader/routines/switcher.rb
@@ -23,28 +23,28 @@ module Yast
       Yast.import "BootCommon"
     end
 
+    # Get method for current bootloader
+    def get_function(method)
+      functions = get_functions(BootCommon.getLoaderType(false))
+      ret = functions[method.to_s.downcase]
+      if !ret && BootCommon.methods.include?(method)
+        ret = BootCommon.method(method)
+      end
+
+      ret
+    end
+
     # Get map of main functions for bootloader
     # @param [String] bootloader string bootloader name
     # @return [Hash] of function
-    def getFunctions(bootloader)
-      return {} if bootloader == nil || bootloader == ""
+    def get_functions(bootloader)
       bl_functions = {
-        "grub2"     => fun_ref(
-          BootGRUB2.method(:GetFunctions),
-          "map <string, any> ()"
-        ),
-        "grub2-efi" => fun_ref(
-          BootGRUB2EFI.method(:GetFunctions),
-          "map <string, any> ()"
-        )
+        "grub2"     => BootGRUB2.method(:GetFunctions),
+        "grub2-efi" => BootGRUB2EFI.method(:GetFunctions)
       }
-      gf = Convert.convert(
-        Ops.get(bl_functions, bootloader),
-        :from => "any",
-        :to   => "map <string, any> ()"
-      )
-      if gf == nil
-        Builtins.y2warning("No bootloader-specific functions specified")
+      gf = bl_functions[bootloader]
+      if !gf
+        Builtins.y2warning("No bootloader-specific functions specified for #{bootloader.inspect}")
         return {}
       end
       gf.call
@@ -53,88 +53,33 @@ module Yast
     # Export bootloader-specific settings
     # @return [Hash] of settings
     def blExport
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "export",
-          fun_ref(BootCommon.method(:Export), "map ()")
-        ),
-        :from => "any",
-        :to   => "map ()"
-      )
-      toEval.call
+      get_function(:Export).call
     end
 
     # Import settings to bootloader
     # @param [Hash] settings map of settingss
     # @return [Boolean] true on success
     def blImport(settings)
-      settings = deep_copy(settings)
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "import",
-          fun_ref(BootCommon.method(:Import), "boolean (map)")
-        ),
-        :from => "any",
-        :to   => "boolean (map)"
-      )
-      toEval.call(settings)
+      get_function(:Import).call(settings)
     end
 
     # Read bootloader-specific settings
     # @param [Boolean] reread boolean true to force rereading the settings from the disk
     # @return [Boolean] true on success
     def blRead(reread, avoid_reading_device_map)
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "read",
-          fun_ref(BootCommon.method(:Read), "boolean (boolean, boolean)")
-        ),
-        :from => "any",
-        :to   => "boolean (boolean, boolean)"
-      )
-      toEval.call(reread, avoid_reading_device_map)
+      get_function(:Read).call(reread, avoid_reading_device_map)
     end
 
     # Reset bootloader-specific settings
     # @param [Boolean] init boolean true if basic initialization of system-dependent
     # settings should be done
     def blReset(init)
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "reset",
-          fun_ref(BootCommon.method(:Reset), "void (boolean)")
-        ),
-        :from => "any",
-        :to   => "void (boolean)"
-      )
-      toEval.call(init)
-
-      nil
+      get_function(:Reset).call(init)
     end
 
     # Propose bootloader settings
     def blPropose
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "propose",
-          fun_ref(BootCommon.method(:Propose), "void ()")
-        ),
-        :from => "any",
-        :to   => "void ()"
-      )
-      toEval.call
-
-      nil
+      get_function(:Propose).call
     end
 
     # Save bootloader cfg. files to the cache of the pluglib
@@ -143,119 +88,46 @@ module Yast
     # @param [Boolean] flush boolean true to flush the settings to the disk
     # @return [Boolean] true on success
     def blSave(clean, init, flush)
-      functions = getFunctions(BootCommon.getLoaderType(false))
-
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "save",
-          fun_ref(
-            BootCommon.method(:Save),
-            "boolean (boolean, boolean, boolean)"
-          )
-        ),
-        :from => "any",
-        :to   => "boolean (boolean, boolean, boolean)"
-      )
-      toEval.call(clean, init, flush)
+      get_function(:Save).call(clean, init, flush)
     end
 
     # Get cfg. summary
     # @return a list summary items
     def blSummary
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "summary",
-          fun_ref(BootCommon.method(:Summary), "list <string> ()")
-        ),
-        :from => "any",
-        :to   => "list <string> ()"
-      )
-      toEval.call
+      get_function(:Summary).call
     end
 
     # Update bootloader-specific settings
     def blUpdate
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "update",
-          fun_ref(BootCommon.method(:Update), "void ()")
-        ),
-        :from => "any",
-        :to   => "void ()"
-      )
-      toEval.call
-
-      nil
+      get_function(:Update).call
     end
 
     # Do the bootloader installation
     # @return [Boolean] true on success
     def blWrite
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(
-          functions,
-          "write",
-          fun_ref(BootCommon.method(:Write), "boolean ()")
-        ),
-        :from => "any",
-        :to   => "boolean ()"
-      )
-      toEval.call
+      get_function(:Write).call
     end
 
     # Get description maps of loader-specific widgets
     # @return a map containing description of all loader-specific widgets
     def blWidgetMaps
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(functions, "widgets"),
-        :from => "any",
-        :to   => "map <string, map <string, any>> ()"
-      )
-      if toEval != nil
-        return toEval.call
-      else
-        return {}
-      end
+      method = get_function(:widgets)
+      method ? method.call : {}
     end
 
     # Get the loader-specific dialogs
     # @return a map of loader-specific dialogs
     def blDialogs
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(functions, "dialogs"),
-        :from => "any",
-        :to   => "map <string, symbol ()> ()"
-      )
-      if toEval != nil
-        return toEval.call
-      else
-        return {}
-      end
+      method = get_function(:dialogs)
+      method ? method.call : {}
     end
 
     # Set section to boot on next reboot for this type of bootloader
     # @param [String] section string section to boot
     # @return [Boolean] true on success
     def blFlagOnetimeBoot(section)
-      functions = getFunctions(BootCommon.getLoaderType(false))
-      toEval = Convert.convert(
-        Ops.get(functions, "flagonetimeboot"),
-        :from => "any",
-        :to   => "boolean (string)"
-      )
-      if toEval != nil
-        return toEval.call(section)
-      else
-        return false
-      end
+      method = get_function(:flagonetimeboot)
+      method ? method.call(section) : false
     end
   end
 end
