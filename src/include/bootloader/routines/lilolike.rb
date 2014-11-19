bf77dbff003a40a71546b10c07a49e30ba735172
Josef Reidinger <jreidinger@suse.cz>
2014-11-19 14:54:18 +0100

    share functionality

--- src/include/bootloader/routines/lilolike.rb
+++ src/include/bootloader/routines/lilolike.rb
@@ -18,6 +18,8 @@
 #
 # $Id$
 #
+require "bootloader/stage1"
+
 module Yast
   module BootloaderRoutinesLilolikeInclude
     def initialize_bootloader_routines_lilolike(include_target)
@@ -65,81 +67,7 @@ module Yast
     # FIXME: replace with grub_ConfigureLocation() when lilo et al. have
     # changed to stop using selected_location and loader_device.
     def ConfigureLocation
-      @selected_location = "mbr" # default to mbr
-      @loader_device = @mbrDisk
-      # check whether the /boot partition
-      #  - is primary:			    is_logical	-> false
-      #  - is on the first disk (with the MBR):  disk_is_mbr -> true
-      tm = Storage.GetTargetMap
-      dp = Storage.GetDiskPartition(BootStorage.BootPartitionDevice)
-      disk = Ops.get_string(dp, "disk", "")
-      disk_is_mbr = disk == @mbrDisk
-      dm = Ops.get_map(tm, disk, {})
-      partitions = Ops.get_list(dm, "partitions", [])
-      is_logical = false
-      extended = nil
-      needed_devices = [BootStorage.BootPartitionDevice]
-      md_info = Md2Partitions(BootStorage.BootPartitionDevice)
-      if md_info != nil && Ops.greater_than(Builtins.size(md_info), 0)
-        disk_is_mbr = false
-        needed_devices = Builtins.maplist(md_info) do |d, b|
-          pdp = Storage.GetDiskPartition(d)
-          p_disk = Ops.get_string(pdp, "disk", "")
-          disk_is_mbr = true if p_disk == @mbrDisk
-          d
-        end
-      end
-      Builtins.y2milestone("Boot partition devices: %1", needed_devices)
-      Builtins.foreach(partitions) do |p|
-        if Ops.get(p, "type") == :extended
-          extended = Ops.get_string(p, "device")
-        elsif Builtins.contains(needed_devices, Ops.get_string(p, "device", "")) &&
-            Ops.get(p, "type") == :logical
-          is_logical = true
-        end
-      end
-      Builtins.y2milestone("/boot is on 1st disk: %1", disk_is_mbr)
-      Builtins.y2milestone("/boot is in logical partition: %1", is_logical)
-      Builtins.y2milestone("The extended partition: %1", extended)
-
-      exit = 0
-      # if is primary, store bootloader there
-      if disk_is_mbr && !is_logical
-        @selected_location = "boot"
-        @loader_device = BootStorage.BootPartitionDevice
-        @activate = true
-        @activate_changed = true
-      elsif Ops.greater_than(Builtins.size(needed_devices), 1)
-        @loader_device = "mbr_md"
-        @selected_location = "mbr_md"
-      end
-
-      if !Builtins.contains(
-          BootStorage.possible_locations_for_stage1,
-          @loader_device
-        )
-        @selected_location = "mbr" # default to mbr
-        @loader_device = @mbrDisk
-      end
-
-      Builtins.y2milestone(
-        "ConfigureLocation (%1 on %2)",
-        @selected_location,
-        @loader_device
-      )
-
-      # set active flag
-      if @selected_location == "mbr"
-        # we are installing into MBR:
-        # if there is an active partition, then we do not need to activate
-        # one (otherwise we do)
-        @activate = Builtins.size(Storage.GetBootPartition(@mbrDisk)) == 0
-      else
-        # if not installing to MBR, always activate
-        @activate = true
-      end
-
-      @selected_location
+      ::Bootloader::Stage1.new.propose.to_s
     end
 
     # Detect /boot and / (root) partition devices
