1a198ef5a89946a8ce39a413c5bd5e609a256c7e
Josef Reidinger <jreidinger@suse.cz>
2015-02-12 17:38:46 +0100

    add test for resume available method

--- test/boot_arch_test.rb
+++ test/boot_arch_test.rb
@@ -10,6 +10,7 @@ describe Yast::BootArch do
 
     allow(Yast::Arch).to receive(:architecture).and_return(arch)
   end
+
   describe ".VgaAvailable" do
     it "returns true if it is on x86_64 architecture" do
       stub_arch("x86_64")
@@ -27,6 +28,32 @@ describe Yast::BootArch do
       stub_arch("s390x")
 
       expect(subject.VgaAvailable).to eq false
+    end
+  end
+
+  describe ".ResumeAvailable" do
+    it "returns true if it is on x86_64 architecture" do
+      stub_arch("x86_64")
+
+      expect(subject.ResumeAvailable).to eq true
+    end
+
+    it "returns true if it is on i386 architecture" do
+      stub_arch("i386")
+
+      expect(subject.ResumeAvailable).to eq true
+    end
+
+    it "returns true if it is on s390 architecture" do
+      stub_arch("s390_64")
+
+      expect(subject.ResumeAvailable).to eq true
+    end
+
+    it "otherwise it returns false" do
+      stub_arch("ppc64")
+
+      expect(subject.ResumeAvailable).to eq false
     end
   end
 end
