1dee56714d937ba1da96bf5f808653fd0b2fa40c
Josef Reidinger <jreidinger@suse.cz>
2015-12-31 14:13:42 +0100

    add documentation

--- src/lib/bootloader/sections.rb
+++ src/lib/bootloader/sections.rb
@@ -2,15 +2,19 @@ require "yast"
 require "yast2/execute"
 
 module Bootloader
+  # Represents available sections and handling of default boot entry
   class Sections
+    # @param [CFA::Grub2::GrubCfg] grub_cfg - loaded parsed grub cfg tree
     def initialize(grub_cfg)
       @data = grub_cfg.sections
     end
 
+    # Gets all available sections
     def all
       @data
     end
 
+    # @return [String] name of default section
     def default
       return @default if @default
 
@@ -20,11 +24,14 @@ module Bootloader
       saved_line || @data.first
     end
 
+    # Sets default section internally
+    # @note to write it to system use #write later
     def default=(value)
       raise "Unknown value #{value.inspect}" unless @data.include?(value)
       @default = value
     end
 
+    # writes default to system making it persistent
     def write
       Yast::Execute.on_target("/usr/sbin/grub2-set-default", @default)
     end
