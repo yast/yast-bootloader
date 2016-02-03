03e8e14c16c760f10c2b57cec46daf3851075bc5
Josef Reidinger <jreidinger@suse.cz>
2016-02-03 15:03:22 +0100

    fix disk change detector

--- src/lib/bootloader/disk_change_detector.rb
+++ src/lib/bootloader/disk_change_detector.rb
@@ -1,23 +1,19 @@
 require "yast"
 
+require "bootloader/udev_mapping"
+
 module Bootloader
   # Task of this class is to detect if user change storage proposal
   # during installation, so bootloader configuration can be invalid.
   class DiskChangeDetector
     include Yast::I18n
 
-    def initialize
-      Yast.import "BootCommon"
+    def initialize(stage1)
       Yast.import "BootStorage"
       Yast.import "Mode"
-      Yast.import "Storage"
 
       textdomain "bootloader"
-
-      mp = Yast::Storage.GetMountPoints
-      @actual_root = mp["/"] ? mp["/"].first : ""
-      @actual_boot = mp["/boot"] ? mp["/boot"].first : actual_root
-      @actual_extended = Yast::BootStorage.extended_partition_for(Yast::BootStorage.BootPartitionDevice)
+      @stage1 = stage1
     end
 
     # Check whether any disk settings for the disks we currently use were changed
@@ -27,25 +23,13 @@ module Bootloader
       ret = []
       return ret if Yast::Mode.config
 
-      if boot_changed?
-        ret << change_message('"/boot"', Yast::BootStorage.BootPartitionDevice)
-      end
-
-      if root_changed?
-        ret << change_message('"/"', Yast::BootStorage.RootPartitionDevice)
-      end
-
-      ret << change_message("MBR", Yast::BootStorage.mbr_disk) if mbr_changed?
-
-      if extended_changed?
-        ret << change_message('"extended partition"', Yast::BootStorage.ExtendedPartitionDevice)
-      end
-
-      if invalid_custom?
-        # TRANSLATORS: %s stands for partition
-        ret <<
-          _("Selected custom bootloader partition %s is not available any more.") %
-            Yast::BootCommon.globals["boot_custom"]
+      @stage1.model.devices.each do |device|
+        if invalid_device?(device)
+          # TRANSLATORS: %s stands for partition
+          ret <<
+            _("Selected bootloader partition %s is not available any more.") %
+              device
+        end
       end
 
       ret
@@ -53,46 +37,10 @@ module Bootloader
 
   private
 
-    attr_reader :actual_boot, :actual_root, :actual_extended
-
-    def change_message(location, device)
-      # TRANSLATORS the %{path} is path where bootloader stage1 is selected to install and
-      # the %{device} is device where it should be, but isn't
-      _("Selected bootloader location %{path} is not on %{device} any more.") %
-        { path: location, device: device }
-    end
-
-    def boot_changed?
-      boot_selected?("boot_boot") &&
-        actual_boot != Yast::BootStorage.BootPartitionDevice
-    end
-
-    def root_changed?
-      boot_selected?("boot_root") &&
-        actual_root != Yast::BootStorage.RootPartitionDevice
-    end
-
-    def mbr_changed?
-      boot_selected?("boot_mbr") &&
-        Yast::BootStorage.find_mbr_disk != Yast::BootStorage.mbr_disk
-    end
-
-    def extended_changed?
-      boot_selected?("boot_extended") &&
-        actual_extended != Yast::BootStorage.ExtendedPartitionDevice
-    end
-
-    def boot_selected?(key)
-      Yast::BootCommon.globals[key] == "true"
-    end
-
-    def invalid_custom?
-      boot_custom = Yast::BootCommon.globals["boot_custom"]
-      return false if !boot_custom || boot_custom.empty?
-
+    def invalid_device?(device)
       all_boot_partitions = Yast::BootStorage.possible_locations_for_stage1
 
-      !all_boot_partitions.include?(boot_custom)
+      !all_boot_partitions.include?(UdevMapping.to_kernel_device(device))
     end
   end
 end
--- test/disk_change_detector_test.rb
+++ test/disk_change_detector_test.rb
@@ -3,75 +3,28 @@
 require_relative "./test_helper"
 
 require "bootloader/disk_change_detector"
+require "bootloader/stage1"
 
 describe Bootloader::DiskChangeDetector do
-  before do
-    Yast.import "Storage"
-    mount_points = { # TODO: full mock
-      "/"     => ["/dev/sda1"],
-      "/boot" => ["/dev/sda2"]
-    }
-    allow(Yast::Storage).to receive(:GetMountPoints).and_return(mount_points)
-
-    allow(Yast::Storage).to receive(:GetTargetMap).and_return({})
-
-    Yast.import "BootStorage"
-    allow(Yast::BootStorage).to receive(:extended_partition_for).and_return(nil)
+  subject do
+    stage1 = Bootloader::Stage1.new
+    stage1.model.add_device("/dev/sda")
+    described_class.new(stage1)
   end
 
-  after do
-    Yast::BootCommon.globals["boot_boot"] = "false"
-    Yast::BootCommon.globals["boot_root"] = "false"
-    Yast::BootCommon.globals["boot_extended"] = "false"
-    Yast::BootCommon.globals["boot_mbr"] = "false"
-    Yast::BootCommon.globals["boot_custom"] = nil
-    Yast::BootCommon.mbrDisk = nil
+  before do
+    allow(Bootloader::UdevMapping).to receive(:to_kernel_device) { |a| a }
   end
 
   describe ".changes" do
     it "returns empty array if disk proposal do not change" do
+      allow(Yast::BootStorage).to receive(:possible_locations_for_stage1).and_return(["/dev/sda", "/dev/sda2"])
       expect(subject.changes).to be_empty
     end
 
-    it "do not crash if separate /boot missing" do
-      mount_points = { # TODO: full mock
-        "/" => ["/dev/sda1"]
-      }
-      allow(Yast::Storage).to receive(:GetMountPoints).and_return(mount_points)
-
-      expect { subject }.to_not raise_error
-    end
-
-    it "returns list containing message with boot if device for /boot changed and stage1 selected for boot" do
-      Yast::BootCommon.globals["boot_boot"] = "true"
-      Yast::BootStorage.BootPartitionDevice = "/dev/sdb1"
-      expect(subject.changes.first).to include('"/boot"')
-    end
-
-    it "returns list containing message with root if device for / changed and stage1 selected for root" do
-      Yast::BootCommon.globals["boot_root"] = "true"
-      Yast::BootStorage.RootPartitionDevice = "/dev/sdb2"
-      expect(subject.changes.first).to include('"/"')
-    end
-
-    it "returns list containing message with extended if extended partition changed and stage1 selected for extended" do
-      Yast::BootCommon.globals["boot_extended"] = "true"
-      allow(Yast::BootStorage).to receive(:extended_partition_for).and_return("/dev/sda4")
-      Yast::BootStorage.ExtendedPartitionDevice = "/dev/sdb2"
-      expect(subject.changes.first).to include('"extended partition"')
-    end
-
-    it "returns list containing message with MBR if boot disk changed and stage1 selected for MBR" do
-      Yast::BootCommon.globals["boot_mbr"] = "true"
-      allow(Yast::BootCommon).to receive(:FindMBRDisk).and_return("/dev/sda")
-      Yast::BootCommon.mbrDisk = "/dev/sdb"
-      expect(subject.changes.first).to include("MBR")
-    end
-
-    it "returns list containing message with custom partition if custom boot disk changed and stage1 selected for custom" do
-      Yast::BootCommon.globals["boot_custom"] = "/dev/sdc"
-      allow(Yast::BootStorage).to receive(:possible_locations_for_stage1).and_return(["/dev/sda", "/dev/sda2"])
-      expect(subject.changes.first).to include("custom bootloader partition")
+    it "returns list containing message if any stage1 device is no longer valid" do
+      allow(Yast::BootStorage).to receive(:possible_locations_for_stage1).and_return(["/dev/sdb", "/dev/sdb2"])
+      expect(subject.changes.first).to include("bootloader partition /dev/sda")
     end
   end
 end
