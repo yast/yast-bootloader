9598c9b8f1f37d7d0efdfc12e6ab7204caefd9c4
Josef Reidinger <jreidinger@suse.cz>
2016-04-13 13:29:34 +0200

    remove test for removed unused method

--- test/boot_arch_test.rb
+++ test/boot_arch_test.rb
@@ -11,26 +11,6 @@ describe Yast::BootArch do
     allow(Yast::Arch).to receive(:architecture).and_return(arch)
   end
 
-  describe ".VgaAvailable" do
-    it "returns true if it is on x86_64 architecture" do
-      stub_arch("x86_64")
-
-      expect(subject.VgaAvailable).to eq true
-    end
-
-    it "returns true if it is on i386 architecture" do
-      stub_arch("i386")
-
-      expect(subject.VgaAvailable).to eq true
-    end
-
-    it "otherwise it returns false" do
-      stub_arch("s390x")
-
-      expect(subject.VgaAvailable).to eq false
-    end
-  end
-
   describe ".ResumeAvailable" do
     it "returns true if it is on x86_64 architecture" do
       stub_arch("x86_64")
