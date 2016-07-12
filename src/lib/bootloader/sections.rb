c9e53811682f1fff9d38252072d8188ed07dfbb9
Josef Reidinger <jreidinger@suse.cz>
2016-07-12 14:07:14 +0200

    write properly default including its submenu (bnc#986005)

--- src/lib/bootloader/sections.rb
+++ src/lib/bootloader/sections.rb
@@ -13,7 +13,8 @@ module Bootloader
     # @param [CFA::Grub2::GrubCfg, nil] grub_cfg - loaded parsed grub cfg tree
     # or nil if not available yet
     def initialize(grub_cfg = nil)
-      @all = grub_cfg ? grub_cfg.sections : []
+      @data = grub_cfg ? grub_cfg.boot_entries : []
+      @all = @data.map { |e| e[:title] }
     end
 
     # @return [String] name of default section
@@ -22,12 +23,9 @@ module Bootloader
 
       return @default = "" if Yast::Stage.initial
 
-      # Execute.on_target can return nil if call failed. It shows users error popup, but bootloader
-      # can continue with empty default section
-      saved = Yast::Execute.on_target("/usr/bin/grub2-editenv", "list", stdout: :capture) || ""
-      saved_line = saved.lines.grep(/saved_entry=/).first
+      default_path = read_default
 
-      @default = saved_line ? saved_line[/saved_entry=(.*)$/, 1] : all.first
+      @default = default_path ? path_to_title(default_path) : all.first
     end
 
     # Sets default section internally
@@ -44,7 +42,31 @@ module Bootloader
     # writes default to system making it persistent
     def write
       return if default.empty?
-      Yast::Execute.on_target("/usr/sbin/grub2-set-default", default)
+
+      Yast::Execute.on_target("/usr/sbin/grub2-set-default", title_to_path(default))
+    end
+
+  private
+
+    def read_default
+      # Execute.on_target can return nil if call failed. It shows users error popup, but bootloader
+      # can continue with empty default section
+      saved = Yast::Execute.on_target("/usr/bin/grub2-editenv", "list", stdout: :capture) || ""
+      saved_line = saved.lines.grep(/saved_entry=/).first
+
+      saved_line ? saved_line[/saved_entry=(.*)$/, 1] : nil
+    end
+
+    def path_to_title(path)
+      entry = @data.find { |e| e[:path] == path }
+
+      entry ? entry[:title] : path
+    end
+
+    def title_to_path(title)
+      entry = @data.find { |e| e[:title] == title }
+
+      entry ? entry[:path] : title
     end
   end
 end
