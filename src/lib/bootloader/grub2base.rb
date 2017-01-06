20ff04c5e035f746c1baf7dd408b9af4b8d8edd6
Josef Reidinger <jreidinger@suse.cz>
2017-01-06 14:22:29 +0100

    when proposing new config set empty new default as we do not know it and lazy read to crash (bsc#1018280)

--- src/lib/bootloader/grub2base.rb
+++ src/lib/bootloader/grub2base.rb
@@ -120,6 +120,9 @@ module Bootloader
 
       propose_serial
 
+      # we proposing new config and we do not know sections yet, so propose empty
+      @sections.default = ""
+
       nil
     end
 
