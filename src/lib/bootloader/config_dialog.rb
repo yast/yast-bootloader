75043181412a3f5ff4d007b0e53f5b9b59fef05c
Ancor Gonzalez Sosa <ancor@suse.de>
2019-11-20 13:50:21 +0100

    Handle NoRoot exception in main workflow (bsc#1124581, bsc#1151748)

--- src/lib/bootloader/config_dialog.rb
+++ src/lib/bootloader/config_dialog.rb
@@ -24,6 +24,11 @@ module Bootloader
 
     def run
       guarded_run
+    rescue ::Bootloader::NoRoot
+      Yast::Report.Error(
+        _("YaST cannot configure the bootloader because it failed to find the root file system.")
+      )
+      :abort
     rescue ::Bootloader::BrokenConfiguration, ::Bootloader::UnsupportedOption => e
       msg = if e.is_a?(::Bootloader::BrokenConfiguration)
         # TRANSLATORS: %s stands for readon why yast cannot process it
