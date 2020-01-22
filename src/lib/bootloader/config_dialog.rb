606118e0f8480d30bab28195b0f5504fa87ad6ad
José Iván López González <jlopez@suse.com>
2020-01-22 08:39:37 +0000

    Revert "Extend error message"
    This reverts commit 6ea8955cd9457af25e660c6135569ba051a679ae.

--- src/lib/bootloader/config_dialog.rb
+++ src/lib/bootloader/config_dialog.rb
@@ -26,8 +26,7 @@ module Bootloader
       guarded_run
     rescue ::Bootloader::NoRoot
       Yast::Report.Error(
-        _("YaST cannot configure the bootloader because it failed to find the root file system.\n" \
-          "This usually happens when there are bind-mounted devices.")
+        _("YaST cannot configure the bootloader because it failed to find the root file system.")
       )
       :abort
     rescue ::Bootloader::BrokenConfiguration, ::Bootloader::UnsupportedOption => e
