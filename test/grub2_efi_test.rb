756d2259121e368d2c247ad04ddedeb99e61282c
Josef Reidinger <jreidinger@suse.cz>
2020-06-12 22:53:31 +0200

    Adapt tests

--- test/grub2_efi_test.rb
+++ test/grub2_efi_test.rb
@@ -36,8 +36,11 @@ describe Bootloader::Grub2EFI do
         .with("/boot/efi").and_return("device" => "/dev/md1")
       allow(Bootloader::Stage1Device).to receive(:new)
         .and_return(double(real_devices: ["/dev/sda", "/dev/sdb"]))
+      allow(Yast::Storage).to receive(:GetDisk)
+        .and_return({"label" => "gpt", "device" => "/dev/sda"},
+          {"label" => "msdos", "device" => "/dev/sdb"})
 
-      expect(subject).to receive(:pmbr_setup).with("/dev/sda", "/dev/sdb")
+      expect(subject).to receive(:pmbr_setup).with("/dev/sda")
 
       subject.write
     end
