b936d0e6d7dcf0413b56a1da013433ba891a68f4
Stefan Schubert <schubi@suse.de>
2024-11-29 16:07:53 +0100

    fixed testcase

--- test/generic_widgets_test.rb
+++ test/generic_widgets_test.rb
@@ -15,3 +15,28 @@ describe Bootloader::LoaderTypeWidget do
 
   include_examples "CWM::ComboBox"
 end
+
+describe Bootloader::PMBRWidget do
+  before do
+    Bootloader::BootloaderFactory.clear_cache
+    Bootloader::BootloaderFactory.current_name = "grub2-bls"
+  end
+
+  it "is initialized to pmbr action" do
+    Bootloader::BootloaderFactory.current.pmbr_action = :add
+    expect(subject).to receive(:value=).with(:add)
+
+    subject.init
+  end
+
+  it "stores pmbr action" do
+    expect(subject).to receive(:value).and_return(:remove)
+    subject.store
+
+    expect(Bootloader::BootloaderFactory.current.pmbr_action).to eq :remove
+  end
+
+  it "offer set, remove and no action options" do
+    expect(subject.items.size).to eq 3
+  end
+end
